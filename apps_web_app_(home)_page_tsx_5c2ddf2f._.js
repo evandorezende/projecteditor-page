2a41330bb18ddb271f98ad7e2a430d62e06dc0ac
Evando <evando@dropdepot.com>
2026-01-21 22:47:09 +0000

    wip

--- apps_web_app_(home)_page_tsx_5c2ddf2f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/apps_web_0e1a25a2._.js",
-  "static/chunks/node_modules__pnpm_976ce09f._.js"
-],
-    source: "dynamic"
-});
